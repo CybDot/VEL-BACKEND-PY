ca5f3aec52bfd4fc3290302cccecb966faa9a499
mrkc101 <alpix.shishir.kc@gmail.com>
2024-10-30 08:05:57 +0545

    updated admin panel

--- VelNote/accounts/admin.py
+++ VelNote/accounts/admin.py
@@ -8,5 +8,13 @@ class VelNoteUserAdmin(UserAdmin):
     list_filter = ('is_active', 'is_staff')
     ordering = ('email',)
     search_fields = ('email', 'username')
+    
+    fieldsets = (
+     (None, {'fields': ('username', 'password')}),
+     ('Personal info', {'fields': ('email',)}),  # Removed profile_picture
+     ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+     ('Important dates', {'fields': ('last_login',)}), 
+    )
+
 
 admin.site.register(VelNoteUser, VelNoteUserAdmin)
